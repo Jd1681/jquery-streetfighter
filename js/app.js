3373910a59ba9aecea78f08a9d7fc609ac4238f4
Jd1681 <jeremy.duprey@gmail.com>
2014-02-24 16:32:16 -0500

    problem- keydown function

--- js/app.js
+++ js/app.js
@@ -26,6 +26,16 @@ $(document).ready(function(){
   	// ryu goes back to his ready position
   });
 });
+
+$(document).keydown(function(event) {
+  	if (event.which == 88 ) {
+  		$('ryu-ready').hide();
+  		$('ryu-throwing').hide();
+  		$('ryu-still').hide();
+  		$('.ryu-cool').show();
+  	};
+});
+
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
