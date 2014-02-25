24c31616c00b302741a91538993488e22ce5c13f
Jd1681 <jeremy.duprey@gmail.com>
2014-02-24 19:04:36 -0500

    new changes

--- js/app_old.js
+++ js/app_old.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
   $('.ryu').mouseenter(function() {
-  	$('.ryu-still').toggleClass('active, inactive');
-  	$('.ryu-ready').toggleClass('active, inactive');
+  	$('.ryu-still').hide();
+  	$('.ryu-ready').show();
   })
   .mouseleave(function() {
-  	$('.ryu-ready').toggleClass('active, inactive');
-  	$('.ryu-still').toggleClass('active, inactive');
+  	$('.ryu-ready').hide();
+  	$('.ryu-still').show();
   })
   .mousedown(function() {
    	playHadouken(); 	
-   	$('.ryu-ready').toggleClass('active, inactive');
-  	$('.ryu-throwing').toggleClass('active, inactive');
-  	$('.hadouken').finish().show();
+   	$('.ryu-ready').hide();
+  	$('.ryu-throwing').show();
+  	$('.hadouken').finish().show()
   	.animate(
   		{'left': '1100px'},
   		500,
   		function()  {
-  			$(this).toggleClass('active, inactive');
+  			$(this).hide();
   			$(this).css('left','500px');
   		});
   })
   .mouseup(function()  {
-  	$('.ryu-throwing').toggleClass('active, inactive');
+  	$('.ryu-throwing').hide();
   	$('.ryu-ready').show();
   	// ryu goes back to his ready position
   });
@@ -29,26 +29,26 @@ $(document).ready(function(){
 
 $(document).keydown(function(event) {
   	if (event.keyCode == 88 ) {
-  		$('.ryu-ready').toggleClass('active, inactive');
+  		$('.ryu-ready').hide();
   		//$('.ryu-throwing').finish().hide();
   		//$('.ryu-still').finish().hide();
-  		$('.ryu-cool').toggleClass('active, inactive');
+  		$('.ryu-cool').show();
       event.preventDefault();
     }
 });
 
 $(document).keyup(function(event) {
     if (event.keyCode == 88 ) {
-      $('.ryu-ready').toggleClass('active, inactive');
+      $('.ryu-ready').show();
       //$('.ryu-throwing').finish().hide();
       //$('.ryu-still').finish().hide();
-      $('.ryu-cool').toggleClass('active, inactive');
+      $('.ryu-cool').hide();
       event.preventDefault();
     }
 });
 
 function playHadouken () {
-	$('#hadouken-sound')[0].volume = 0.2;
+	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
 }
